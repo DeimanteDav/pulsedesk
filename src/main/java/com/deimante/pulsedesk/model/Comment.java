0ba8f2449369ea78757f146b62f89908a225b1d3
DeimanteDav <deimante.davi@gmail.com>
2026-03-23 19:06:31 +0200

    Refactor Comment and Ticket models to include Lombok annotations for boilerplate code reduction

--- src/main/java/com/deimante/pulsedesk/model/Comment.java
+++ src/main/java/com/deimante/pulsedesk/model/Comment.java
@@ -3,12 +3,20 @@ package com.deimante.pulsedesk.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Comment {
     @Id @GeneratedValue
     private Long id;
-    private String title;
-    private String category;
+    private String text;
 
+    public Comment(String text) {
+        this.text = text;
+    }
 }
